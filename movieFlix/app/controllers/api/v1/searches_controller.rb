29697749b838dee6bc90c34284368f3cef9d7f4b
eduardotecnologo <eduardotecnologo@hotmail.com>
2018-07-25 20:05:39 -0300

    Create Services

--- movieFlix/app/controllers/api/v1/searches_controller.rb
+++ movieFlix/app/controllers/api/v1/searches_controller.rb
@@ -1,4 +1,13 @@
-class Api::V1::SearchesController < ApplicationController
-  def index
-  end
+class​ ​Api::V1::SearchesController​ < ApplicationController​​
+  
+  def ​​index   
+    check_search_value ​and​ (​return​​ if​ performed?)   
+    search = PgSearch.multisearch(params[​:value​]).order(​"created_atDESC"​)   
+      ender ​json:
+    Api::V1::WatchableSerializer.new(search.map(&​:searchable​)).serialized_json
+  ​end​ 
+  private​​def​​check_search_value​
+    if​ params[​:value​].present? && params[​:value​].length < 3 render ​json:​ { ​errors:​​"Parameter :value must have at least 3characters"​ }​
+    end
+  ​end
 end
